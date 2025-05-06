44855c0b64bba45097c6f4782e899f0fc9f7352e
Ritesh Singh <94886442+Ritesh8107@users.noreply.github.com>
2025-05-06 23:37:07 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import svgr from '@svgr/rollup';
+import svgr from '@svgr/rollup'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/', // ✅ Important for Vercel
   plugins: [react(), svgr()],
   server: {
     port: 3000,
